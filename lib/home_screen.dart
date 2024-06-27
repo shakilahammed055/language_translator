ea5dd662edd40d42c0093b2cfbd41a3bcbb4ee00
shakil ahammed <shakilahammed055@gmail.com>
2024-06-27 23:10:12 +0600

    Nothing

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:translator/translator.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,6 +15,14 @@ class _HomeScreenState extends State<HomeScreen> {
   var destinationLanguage = "to";
   var output = "";
   TextEditingController languageController = TextEditingController();
+
+  void translate(String src, String dest, String input) async{
+    GoogleTranslator translator = new GoogleTranslator();
+    var translation = await translator.translate(input,from: src,to: dest);
+    setState(() {
+      output= translation.text.toString();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
